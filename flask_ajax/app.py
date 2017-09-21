925370edaa05fa154ee298cdf065ab3a443ecd06
jihu9 <jihu9@163.com>
2017-09-22 07:12:32 +0800

    add autohome data and show index echarts

--- flask_ajax/app.py
+++ flask_ajax/app.py
@@ -3,8 +3,9 @@
 # __author:physics
 # app.py
 
-import sqlite3
+import sqlite3, time
 from flask import Flask, request, render_template, jsonify
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 
@@ -23,7 +24,20 @@ def query_db(query, args=(), one=False):
     db.close()
     return (rv[0] if rv else None) if one else rv
 
-
+def get_gs8():
+    db = TinyDB('gs8.json')
+    q = Query()
+    lst = []
+    for i in [str(a)[1:] for a in range(101,113,1)]:
+        item = db.search(q.create_time.search('2017-{}-*'.format(i)))
+        if len(item) > 0:
+            lst.append(['{}月'.format(int(i)),len(item)])
+        else:
+            lst.append(['{}月'.format(int(i)), 0])
+    db.close()
+    print(lst)
+    return lst
+# get_gs8()
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
@@ -31,13 +45,24 @@ def index():
 
 @app.route("/weather", methods=["POST","GET"])
 def weather():
-    if request.method == "GET":
+    if request.method == "POST":
         res = query_db("SELECT * FROM weather")
 
     return jsonify(month=[x[0] for x in res],
                    evaporation=[x[1] for x in res],
                    precipitation=[x[2] for x in res])
 
+# result = get_gs8()
+@app.route("/gs8",methods=['POST','GET'])
+def gs8():
+    if request.method == 'POST':
+        t1 = time.time()
+        res = get_gs8()
+        print('it cost {} s!'.format(time.time()-t1))
+    return jsonify(month=[x[0] for x in res],
+                   nums = [x[1] for x in res])
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='127.0.0.1',port=8080)
